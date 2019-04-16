e8068a71f6733f220bfe1f8cba693b794d5c6221
August <mingxin.android@gmail.com>
2019-04-16 19:44:49 +0800

    uboot: update build script

--- u-boot-rt/build_bananapi_m4_all.sh
+++ u-boot-rt/build_bananapi_m4_all.sh
@@ -4,6 +4,7 @@ mkdir -p DVRBOOT_OUT/hw_setting
 make rtd1395_bananapi_defconfig;
 make
 
-cp ./examples/flash_writer_vm/install_a/hw_setting.bin ./DVRBOOT_OUT/hw_setting/
-cp ./examples/flash_writer_vm/Bind/emmc.bind.bin ./DVRBOOT_OUT/hw_setting/
-cp ./examples/flash_writer_vm/dvrboot.exe.bin ./DVRBOOT_OUT/
+# emmc bootup
+cp ./examples/flash_writer_vm/install_a/hw_setting.bin ./DVRBOOT_OUT/hw_setting/hw_setting.bin
+cp ./examples/flash_writer_vm/Bind/emmc.bind.bin ./DVRBOOT_OUT/hw_setting/recovery.bin
+cp ./examples/flash_writer_vm/dvrboot.exe.bin ./DVRBOOT_OUT/bootloader.bin
